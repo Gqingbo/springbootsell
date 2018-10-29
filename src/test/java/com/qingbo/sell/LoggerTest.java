d8c9965ffd81506deb321b8c3b1ec711c756d50b
qingbo.gao <qingbo.gao@nanguache.com>
2018-10-29 13:33:17 +0800

    日志配置

---
+++ src/test/java/com/qingbo/sell/LoggerTest.java
@@ -0,0 +1,29 @@
+package com.qingbo.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Auther: gaoqingbo
+ * @Date: 2018/10/29 10:20
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+    private Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void test1(){
+        logger.info("info....");
+        logger.error("error.....");
+        logger.debug("debugger....");
+    }
+}
